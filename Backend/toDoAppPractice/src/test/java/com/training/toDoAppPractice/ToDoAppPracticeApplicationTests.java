4e8771e3281040656c72451f203d60ae4b1cc326
SantiagoGM19 <santigm1905@gmail.com>
2022-05-07 09:15:10 -0500

    created my entity

---
+++ Backend/toDoAppPractice/src/test/java/com/training/toDoAppPractice/ToDoAppPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.toDoAppPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
